cb9132171b3c1b73105e8df2dd5d6b7dee183696
Len Smith <lrsmith@lrsmith.dev>
2024-07-20 20:56:14 -0700

    feat: Initial implementation of Backstage Group Entity Object

--- backstage/__init__.py
+++ backstage/__init__.py
@@ -3,15 +3,14 @@ import yaml
 
 class Backstage:
 
-    def __init__(self, kind, metadata, apiVersion=None ):
+    def __init__(self, kind, name, apiVersion=None ):
 
-        # Check for mandatory, for all entities, in metadata
-        if "name" not in metadata:
-            raise ValueError("name not defined in metadata")
 
-        self.__metadata = metadata
         self.__apiVersion = 'backstage.io/v1alpha1' if apiVersion is None else apiVersion
         self.__kind = kind
+
+        self.__metadata = {}
+        self.__metadata['name'] = name
 
     @property 
     def apiversion(self): 
@@ -29,8 +28,76 @@ class Backstage:
         tmp = {}
         tmp['metadata'] = self.__metadata
 
+        if hasattr(self,"spec"):
+            tmp['spec'] = self.spec
+
         return (f'apiVersion: {self.apiversion}\n'
                 f'kind: {self.kind}\n' + 
                 yaml.dump(tmp) 
                 )
 
+# https://backstage.io/docs/features/software-catalog/descriptor-format#kind-group
+class BackstageGroup(Backstage):
+    def __init__(self,
+                 name,
+                 type,
+                 parent=None,       # Optional field
+                 apiversion=None,
+                 children=[],
+                 displayname=None,  # Optional field
+                 email=None,        # Optional field
+                 members=[],        # Optional field
+    ):
+
+        self.__spec             = {}
+        self.__spec['type']     = type
+        self.__spec['children'] = children
+
+        if parent is not None:
+            self.__spec['parent']  = parent
+
+        if members != []:
+            self.__spec['members'] = members
+
+        if displayname is not None:
+            if 'profile' not in self.__spec:
+                self.__spec['profile'] = {}
+            self.__spec['profile']['displayName'] = displayname
+
+        if email is not None:
+            if 'profile' not in self.__spec:
+                self.__spec['profile'] = {}
+            self.__spec['profile']['email'] = email
+
+
+        super().__init__(name=name,
+                         kind="group")
+
+    @property
+    def type(self): 
+        return self.__spec['type']
+
+    @property
+    def children(self): 
+        return self.__spec['children']
+
+    @property
+    def displayname(self): 
+        return self.__spec['profile']['displayName']
+
+    @property
+    def email(self): 
+        return self.__spec['profile']['email']
+
+    @property
+    def members(self): 
+        return self.__spec['members']
+
+    @property
+    def parent(self): 
+        return self.__spec['parent']
+
+
+    @property
+    def spec(self): 
+        return self.__spec
--- tests/test_class_backstageGroup.py
+++ tests/test_class_backstageGroup.py
@@ -1,1 +1,156 @@
 import backstage
+import pytest
+
+
+def test_backstageGroupClass_requiredFields_noChildren(capsys):
+    obj = backstage.BackstageGroup(name="o11y",
+                                   type="team")
+    print(obj)
+    captured = capsys.readouterr()
+    expected = r"""apiVersion: backstage.io/v1alpha1
+kind: group
+metadata:
+  name: o11y
+spec:
+  children: []
+  type: team
+
+"""
+    assert obj.type == "team"
+    assert obj.children == []
+    assert captured.out == expected
+
+def test_backstageGroupClass_requiredFields_withChildren(capsys):
+    obj = backstage.BackstageGroup(name="o11y",
+                                   type="team",
+                                   children= ['team-logging','team-metrics','team-traces'])
+    print(obj)
+    captured = capsys.readouterr()
+    expected = r"""apiVersion: backstage.io/v1alpha1
+kind: group
+metadata:
+  name: o11y
+spec:
+  children:
+  - team-logging
+  - team-metrics
+  - team-traces
+  type: team
+
+"""
+    assert captured.out == expected
+    assert obj.type == "team"
+    assert obj.children == [ 'team-logging', 'team-metrics', 'team-traces' ]
+
+def test_backstageGroupClass_withParent(capsys):
+    obj = backstage.BackstageGroup(name="o11y",
+                                   type="team",
+                                   parent = "engineering" )
+    print(obj)
+    captured = capsys.readouterr()
+    expected = r"""apiVersion: backstage.io/v1alpha1
+kind: group
+metadata:
+  name: o11y
+spec:
+  children: []
+  parent: engineering
+  type: team
+
+"""
+    assert captured.out == expected
+    assert obj.parent == "engineering"
+
+def test_backstageGroupClass_withMembers(capsys):
+    obj = backstage.BackstageGroup(name="o11y",
+                                   type="team",
+                                   members = [ 'John Doe', 'Jane Doe'] )
+    print(obj)
+    captured = capsys.readouterr()
+    expected = r"""apiVersion: backstage.io/v1alpha1
+kind: group
+metadata:
+  name: o11y
+spec:
+  children: []
+  members:
+  - John Doe
+  - Jane Doe
+  type: team
+
+"""
+    assert obj.members == [ 'John Doe', 'Jane Doe']
+    assert captured.out == expected
+
+
+
+def test_backstageGroupClass_withDisplayname(capsys):
+    obj = backstage.BackstageGroup(name="o11y",
+                                   type="team",
+                                   displayname = "The Observability Team" )
+    print(obj)
+    captured = capsys.readouterr()
+    expected = r"""apiVersion: backstage.io/v1alpha1
+kind: group
+metadata:
+  name: o11y
+spec:
+  children: []
+  profile:
+    displayName: The Observability Team
+  type: team
+
+"""
+    assert obj.displayname == "The Observability Team"
+    assert captured.out == expected
+
+def test_backstageGroupClass_withEmail(capsys):
+    obj = backstage.BackstageGroup(name="o11y",
+                                   type="team",
+                                   email = "o11y@big.brother" )
+    print(obj)
+    captured = capsys.readouterr()
+    expected = r"""apiVersion: backstage.io/v1alpha1
+kind: group
+metadata:
+  name: o11y
+spec:
+  children: []
+  profile:
+    email: o11y@big.brother
+  type: team
+
+"""
+    assert obj.email == "o11y@big.brother"
+    assert captured.out == expected
+
+def test_backstageGroupClass(capsys):
+    obj = backstage.BackstageGroup(name="o11y",
+                                   type="team",
+                                   members = [ 'John Doe', 'Jane Doe'],
+                                   parent = "engineering",
+                                   displayname = "The Observability Team",
+                                   children= ['team-logging','team-metrics','team-traces'],
+                                   email = "o11y@big.brother" )
+    print(obj)
+    captured = capsys.readouterr()
+    expected = r"""apiVersion: backstage.io/v1alpha1
+kind: group
+metadata:
+  name: o11y
+spec:
+  children:
+  - team-logging
+  - team-metrics
+  - team-traces
+  members:
+  - John Doe
+  - Jane Doe
+  parent: engineering
+  profile:
+    displayName: The Observability Team
+    email: o11y@big.brother
+  type: team
+
+"""
+    assert captured.out == expected
